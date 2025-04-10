05bd569f419cf74fd086148992802c943e75e212
digiot <digiot.abc@gmail.com>
2025-04-10 11:10:14 +0900

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/facetherapie-user-liff/', // ← GitHubリポジトリ名に合わせて変更
 	plugins: [tailwindcss(), sveltekit()]
 });
